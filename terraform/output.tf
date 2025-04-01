2fe73c528034e0bba7375f1d79d9a14a7a4e7eb1
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-04-01 20:47:49 -0300

    feat: adding s3 bucket creation

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,9 @@ resource "aws_ssm_parameter" "loki_elasticache_endpoint" {
   type  = "String"
   value = aws_elasticache_cluster.loki_memcached.cache_nodes[0].address
 }
+
+resource "aws_ssm_parameter" "video_file_store_bucket_name" {
+  name  = "/fiap-hackathon/video-file-store-bucket"
+  type  = "String"
+  value = aws_s3_bucket.video_file_store.bucket
+}
