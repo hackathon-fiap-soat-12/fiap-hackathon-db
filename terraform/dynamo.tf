c866642c714774848c03b3fca85a3613ca25ca6e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2025-04-11 20:44:22 -0300

    feat: Add columns in dynamodb

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,13 +1,31 @@
 resource "aws_dynamodb_table" "video_entity" {
   name           = "video_entity"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-
-  hash_key = "id"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "UserCreatedAtIndex"
+    hash_key           = "userId"
+    range_key          = "createdAt"
+    projection_type    = "ALL"
+    read_capacity      = 5
+    write_capacity     = 5
   }
 }
