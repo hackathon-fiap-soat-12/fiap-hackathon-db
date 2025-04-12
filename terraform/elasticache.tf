72941e17e0d9a38180465cf48bb15cddba9fa2cf
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-04-12 03:40:36 -0300

    feat: updating s3 configuration

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -1,37 +1,37 @@
-resource "aws_security_group" "elasticache_sg" {
-  name        = var.elasticache_security_group_name
-  description = "Security group for ElastiCache Memcached"
-  vpc_id      = data.aws_vpc.selected_vpc.id
+# resource "aws_security_group" "elasticache_sg" {
+#   name        = var.elasticache_security_group_name
+#   description = "Security group for ElastiCache Memcached"
+#   vpc_id      = data.aws_vpc.selected_vpc.id
 
-  ingress {
-    from_port = 11211
-    to_port   = 11211
-    protocol  = "tcp"
-    self      = true
-  }
+#   ingress {
+#     from_port = 11211
+#     to_port   = 11211
+#     protocol  = "tcp"
+#     self      = true
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_elasticache_cluster" "loki_memcached" {
-  cluster_id           = "loki-memcached"
-  engine               = "memcached"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 2
-  parameter_group_name = "default.memcached1.6"
-  port                 = 11211
-  subnet_group_name    = aws_elasticache_subnet_group.loki_subnet_group.name
-  security_group_ids   = [aws_security_group.elasticache_sg.id]
-}
+# resource "aws_elasticache_cluster" "loki_memcached" {
+#   cluster_id           = "loki-memcached"
+#   engine               = "memcached"
+#   node_type            = "cache.t3.micro"
+#   num_cache_nodes      = 2
+#   parameter_group_name = "default.memcached1.6"
+#   port                 = 11211
+#   subnet_group_name    = aws_elasticache_subnet_group.loki_subnet_group.name
+#   security_group_ids   = [aws_security_group.elasticache_sg.id]
+# }
 
-resource "aws_elasticache_subnet_group" "loki_subnet_group" {
-  name       = var.elasticache_subnet_group_name
-  subnet_ids = [for subnet in data.aws_subnet.selected_subnets : subnet.id]
-}
+# resource "aws_elasticache_subnet_group" "loki_subnet_group" {
+#   name       = var.elasticache_subnet_group_name
+#   subnet_ids = [for subnet in data.aws_subnet.selected_subnets : subnet.id]
+# }
 
 
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,18 @@
 resource "aws_s3_bucket" "video_file_store" {
-  bucket = "video-file-store-${data.aws_caller_identity.current.account_id}"
+  bucket = "fiap-alquimia-dos-frames"
 
   force_destroy = true
+}
+
+
+resource "aws_s3_bucket_cors_configuration" "cors_rules" {
+  bucket = aws_s3_bucket.video_file_store.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag", "x-amz-meta-content-type"]
+    max_age_seconds = 3000
+  }
 }
