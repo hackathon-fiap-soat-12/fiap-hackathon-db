03cfcf656183579519728d4960feaf2feb94dd33
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-04-12 03:50:39 -0300

    feat: removing comments

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,11 +4,11 @@ resource "aws_ssm_parameter" "sonarqube_rds_endpoint" {
   value = "jdbc:postgresql://${aws_db_instance.sonarqube_db_postgres.endpoint}/${var.sonarqube_db_name}"
 }
 
-# resource "aws_ssm_parameter" "loki_elasticache_endpoint" {
-#   name  = "/fiap-hackathon/loki-elasticache-endpoint"
-#   type  = "String"
-#   value = aws_elasticache_cluster.loki_memcached.cache_nodes[0].address
-# }
+resource "aws_ssm_parameter" "loki_elasticache_endpoint" {
+  name  = "/fiap-hackathon/loki-elasticache-endpoint"
+  type  = "String"
+  value = aws_elasticache_cluster.loki_memcached.cache_nodes[0].address
+}
 
 resource "aws_ssm_parameter" "video_file_store_bucket_name" {
   name  = "/fiap-hackathon/video-file-store-bucket"
